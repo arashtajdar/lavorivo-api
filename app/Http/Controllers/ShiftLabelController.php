aa939c72c998da8f19d6fda98ff43a4ad27731a8
Arash Tajdar <arashtajdar@gmail.com>
2024-12-14 17:35:08 +0100

    update

---
+++ app/Http/Controllers/ShiftLabelController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ShiftLabel;
+use Illuminate\Http\Request;
+
+class ShiftLabelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ShiftLabel $shiftLabel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ShiftLabel $shiftLabel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ShiftLabel $shiftLabel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ShiftLabel $shiftLabel)
+    {
+        //
+    }
+}
