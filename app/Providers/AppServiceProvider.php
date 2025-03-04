fb21441d513ea046e237edd2fa8eaf481562f3ae
Arash Tajdar <arashtajdar@gmail.com>
2025-03-04 11:45:26 +0100

    Add log viewer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Opcodes\LogViewer\Facades\LogViewer;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        LogViewer::auth(function ($request) {
+            $authorizedEmails = [];
+            array_push($authorizedEmails, 'arash.tajdar@gmail.com');
+            return $request->user() && in_array($request->user()->email, $authorizedEmails);
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -32,3 +36,24 @@ Route::get('/reset-password/{token}', function ($token) {
         </html>
     ', 200, ['Content-Type' => 'text/html']);
 })->middleware('guest')->name('password.reset');
+
+Route::get('/login', function () {
+    return '<form method="POST" action="/login">
+                ' . csrf_field() . '
+                <input type="email" name="email" placeholder="Email">
+                <input type="password" name="password" placeholder="Password">
+                <button type="submit">Login</button>
+            </form>';
+});
+
+Route::post('/login', function (Request $request) {
+    $user = User::where('email', $request->email)->first();
+
+    if ($user && Hash::check($request->password, $user->password)) {
+        Auth::login($user);
+        return redirect('/log-viewer');
+    }
+
+    return 'Invalid credentials';
+});
+
