19fe80e6deee50e7c17c650086d8bd18eec1876e
arashtajdar <arashtajdar@gmail.com>
2025-04-14 12:02:47 +0200

    Refactor ShiftLabelController

---
+++ app/Services/ShiftLabelService.php
@@ -0,0 +1,50 @@
+<?php
+namespace App\Services;
+
+use App\Models\ShiftLabel;
+use App\Repository\ShiftLabelRepository;
+use Illuminate\Support\Collection;
+
+class ShiftLabelService
+{
+    protected ShiftLabelRepository $shiftLabelRepository;
+
+    public function __construct(ShiftLabelRepository $shiftLabelRepository)
+    {
+        $this->shiftLabelRepository = $shiftLabelRepository;
+    }
+
+    public function getShiftLabelsByShopId(int $shopId): Collection
+    {
+        return $this->shiftLabelRepository->getAllByShopId($shopId);
+    }
+
+    public function getActiveShiftLabelsByShopId(int $shopId): Collection
+    {
+        return $this->shiftLabelRepository->getAllActiveByShopId($shopId);
+    }
+
+    public function createShiftLabel(array $data): ShiftLabel
+    {
+        return $this->shiftLabelRepository->create($data);
+    }
+
+    public function updateShiftLabel(int $id, array $data): bool
+    {
+        return $this->shiftLabelRepository->updateById($id, $data);
+    }
+
+    public function deleteShiftLabel(int $id): bool
+    {
+        return $this->shiftLabelRepository->deleteById($id);
+    }
+
+    public function getAllShiftLabelsForUser(int $userId): Collection
+    {
+        return $this->shiftLabelRepository->getShiftLabelsByUserId($userId);
+    }
+    public function findById(int $id): ?ShiftLabel
+    {
+        return $this->shiftLabelRepository->findById($id);
+    }
+}
